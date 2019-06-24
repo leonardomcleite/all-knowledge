f02d0d3c148132a6c9638cbdf3be4529de969a19
leonardo.leite <leonardo.leite@sinqia.com.br>
2019-06-23 21:31:50 -0300

    build.sh

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-
+PATH=~/projetos/ang7-all-knowledge/
 
 function reset() {
     echo "Removendo imagens anteriores..."
 
     echo "... Removendo node_modules"
-    rm -rf ../node_modules/
+    rm -rf ${PATH}/node_modules/
 
     echo "... Removendo package-lock.json"
-    rm -rf ../package-lock.json
+    rm -rf ${PATH}/package-lock.json
 
     echo "... Removendo dist"
-    rm -rf ../docs/docker/dist
+    rm -rf ${PATH}/docs/docker/dist
 
     echo "... Removendo container all-knowledge"
     docker rm --force /all-knowledge
@@ -29,10 +29,10 @@ function deploy() {
     npm run buildprod
 
     echo "... Realizando o build no docker: docker build -t all-knowledge docs/docker/ "
-    docker build -t all-knowledge ../docs/docker/
+    docker build -t all-knowledge ${PATH}/docs/docker/
 
     echo "... subindo container build no docker: docker run -d --name all-knowledge -it -p 80:80/tcp --privileged=true --env-file=docs/docker-conf/APP.env all-knowledge"
-    docker run -d --name all-knowledge -it -p 80:80/tcp --privileged=true --env-file=../docs/docker-conf/APP.env all-knowledge
+    docker run -d --name all-knowledge -it -p 80:80/tcp --privileged=true --env-file=${PATH}/docs/docker-conf/APP.env all-knowledge
 
     echo "SUCCESS BUILD"
 }
