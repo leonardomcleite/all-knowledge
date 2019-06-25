f94dc1011b907703d92a0ab922a1b40390d35cba
leonardo.leite <leonardo.leite@sinqia.com.br>
2019-06-25 15:20:03 -0300

    router

--- src/app/shared/components/nav-header/nav-header.module.ts
+++ src/app/shared/components/nav-header/nav-header.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NavHeaderComponent } from './nav-header.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
   ],
   imports: [
     CommonModule,
+    RouterModule,
 
     MatButtonModule,
     MatIconModule,
