b67fac6a0a9737af3e25fb0e0e295ab9da5d7356
leonardo.leite <leonardo.leite@sinqia.com.br>
2019-06-30 12:38:55 -0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,59 +7,59 @@ import { InternationalizationService } from './core/services/internationalizatio
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent {
 
-  sideOpened = true;
+  // sideOpened = true;
 
-  mobileQuery: MediaQueryList;
-  desktopQuery: MediaQueryList;
-  private desktopQueryListener: () => void;
-  private mobileQueryListener: () => void;
+  // mobileQuery: MediaQueryList;
+  // desktopQuery: MediaQueryList;
+  // private desktopQueryListener: () => void;
+  // private mobileQueryListener: () => void;
 
-  closedByMobile: boolean;
+  // closedByMobile: boolean;
 
-  constructor(
-    public changeDetectorRef: ChangeDetectorRef,
-    public media: MediaMatcher,
-    private internationalizationService: InternationalizationService,
-  ) {
-    this.internationalizationService.init();
+  // constructor(
+  //   public changeDetectorRef: ChangeDetectorRef,
+  //   public media: MediaMatcher,
+  //   private internationalizationService: InternationalizationService,
+  // ) {
+  //   this.internationalizationService.init();
 
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    if (this.mobileQuery.matches) {
-      this.sideOpened = false;
-      this.closedByMobile = true;
-    }
-    this.mobileQueryListener = () => {
-      if (this.mobileQuery.matches) {
-        this.closedByMobile = true;
-      }
-      changeDetectorRef.detectChanges();
-    };
-    this.mobileQuery.addEventListener('change', this.mobileQueryListener);
+  //   this.mobileQuery = media.matchMedia('(max-width: 600px)');
+  //   if (this.mobileQuery.matches) {
+  //     this.sideOpened = false;
+  //     this.closedByMobile = true;
+  //   }
+  //   this.mobileQueryListener = () => {
+  //     if (this.mobileQuery.matches) {
+  //       this.closedByMobile = true;
+  //     }
+  //     changeDetectorRef.detectChanges();
+  //   };
+  //   this.mobileQuery.addEventListener('change', this.mobileQueryListener);
 
-    this.desktopQuery = media.matchMedia('(min-width: 610px)');
-    if (this.desktopQuery.matches) {
-      this.closedByMobile = false;
-    }
-    this.desktopQueryListener = () => {
-      if (this.desktopQuery.matches) {
-        this.closedByMobile = false;
-      }
-      changeDetectorRef.detectChanges();
-    };
-    this.desktopQuery.addEventListener('change', this.desktopQueryListener);
-  }
+  //   this.desktopQuery = media.matchMedia('(min-width: 610px)');
+  //   if (this.desktopQuery.matches) {
+  //     this.closedByMobile = false;
+  //   }
+  //   this.desktopQueryListener = () => {
+  //     if (this.desktopQuery.matches) {
+  //       this.closedByMobile = false;
+  //     }
+  //     changeDetectorRef.detectChanges();
+  //   };
+  //   this.desktopQuery.addEventListener('change', this.desktopQueryListener);
+  // }
 
-  ngOnDestroy(): void {
-    this.mobileQuery.removeEventListener('change', this.mobileQueryListener);
-    this.desktopQuery.removeEventListener('change', this.desktopQueryListener);
-  }
+  // ngOnDestroy(): void {
+  //   this.mobileQuery.removeEventListener('change', this.mobileQueryListener);
+  //   this.desktopQuery.removeEventListener('change', this.desktopQueryListener);
+  // }
 
-  closeSidenav(sidenav) {
-    if (this.closedByMobile) {
-      sidenav.close();
-    }
-  }
+  // closeSidenav(sidenav) {
+  //   if (this.closedByMobile) {
+  //     sidenav.close();
+  //   }
+  // }
 
 }
